b7b6a281e642c2dd44feac39796a2b365c5e4828
yilingliu <liu61024@gmail.com>
2025-02-24 03:37:37 +0800

    commit2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/ 你的 repo 名稱 /" : "/",
 })
